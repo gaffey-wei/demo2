56ac8df346809cb7eba2f9d60cbbf14ec9e63d3c
gaffey.wei <gaffey.wei@wecosmos.com>
2024-02-22 23:53:57 +0800

    test

---
+++ demo.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import gradio as gr
+
+def greet(name):
+    return "Hello " + name + "!"
+
+demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+
+demo.launch(server_name="0.0.0.0")
+
+
+# In[ ]:
+
+
+
+
